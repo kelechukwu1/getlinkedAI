a2287939b383d2fee183ebde97b6f6808a6023ea
thejuggernaut01 <adewaleolanrewaju2002@gmail.com>
2023-09-21 00:01:57 +0100

    register page 98% done

---
+++ src/components/BackHome.jsx
@@ -0,0 +1,29 @@
+"use client";
+
+import React from "react";
+import { useRouter } from "next/navigation";
+
+import Image from "next/image";
+import Ellipse from "../../public/Ellipse 4.svg";
+
+const BackHome = () => {
+  const router = useRouter();
+  return (
+    <div className="mb-14 relative md:hidden">
+      <Image
+        src={Ellipse}
+        alt=""
+        width={35}
+        className="relative cursor-pointer"
+      />
+      <button
+        onClick={() => router.push("/")}
+        className="text-white absolute top-[9%] left-[3%] text-lg"
+      >
+        {"<"}
+      </button>
+    </div>
+  );
+};
+
+export default BackHome;
