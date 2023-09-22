5843b9189294d19fa04c462bfd419e73a8a47f19
thejuggernaut01 <adewaleolanrewaju2002@gmail.com>
2023-09-22 05:37:20 +0100

    finished contact page, 90% done with register page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -6,106 +6,121 @@ import star from "../../../public/star.png";
 import stargray from "../../../public/stargray.png";
 import sataGra from "../../../public/sata gra.png";
 import Image from "next/image";
-import Button from "@/components/Button";
 
 const page = () => {
-	return (
-		<main className=" w-[80%] mx-auto">
-			<section className="md:flex items-center gap-10">
-				<div className="hidden md:block md:flex-1">
-					<h2 className="text-[32px] font-semibold text-color-2">
-						Get in touch
-					</h2>
-					<div className="text-white space-y-5">
-						<p>Contact information</p>
-						<div>
-							<p>27, Alara Street</p>
-							<p>Yaba 100012</p>
-							<p>Lagos State</p>
-						</div>
-						<p>Call Us : 07067981819</p>
-						<p>we are open from Monday-Friday 08:00am - 05:00pm</p>
-					</div>
+  return (
+    <main className=" w-[80%] mx-auto md:mt-10">
+      <section className="md:flex items-center gap-10">
+        <div className="relative md:flex-1">
+          <Image
+            src={sataGra}
+            alt=""
+            width={15}
+            className="absolute -top-[5.5rem] left-4"
+          />
+          <div className="hidden md:block md:flex-1">
+            <h2 className="text-[32px] font-semibold text-color-2">
+              Get in touch
+            </h2>
+            <div className="text-white space-y-5">
+              <p>Contact information</p>
+              <div>
+                <p>27, Alara Street</p>
+                <p>Yaba 100012</p>
+                <p>Lagos State</p>
+              </div>
+              <p>Call Us : 07067981819</p>
+              <p>we are open from Monday-Friday 08:00am - 05:00pm</p>
+            </div>
 
-					<p className="text-color-2 mt-10 mb-2">Share on</p>
-					<div className="mb-10 flex">
-						<Socials />
-					</div>
-				</div>
+            <p className="text-color-2 mt-10 mb-2">Share on</p>
+            <div className="mb-10 flex">
+              <Socials />
+            </div>
+          </div>
+        </div>
 
-				<div className="w-[85%] md:w-full md:flex-1 mx-auto mt-10 md:border md:p-14 lg:p-10 md:rounded-md md:border-transparent md:shadow-2xl md:shadow-[0px_4px_4px_0px rgba(0, 0, 0, 0.25)]">
-					<BackHome />
-					<div className="text-color-2 text-2xl md:text-lg font-semibold ">
-						<h2>Questions or need assistance?</h2>
-						<h2>Let us know about it</h2>
-					</div>
-					<p className="text-white my-7">
-						Email us below to any question related to our event
-					</p>
+        <div className="w-[95%] md:w-full md:flex-1 mx-auto mt-10 md:border md:p-14 lg:p-10 md:rounded-md md:border-[#FFFFFF08] md:shadow-2xl md:bg-[#FFFFFF08]">
+          <BackHome />
+          <div className="text-color-2 text-2xl md:text-lg font-semibold relative">
+            <h2>Questions or need assistance?</h2>
+            <h2>Let us know about it</h2>
+            <Image
+              src={stargray}
+              alt=""
+              width={15}
+              className="absolute top-9 right-4 md:-top-28 md:-right-9 "
+            />
+            <Image
+              src={sataGra}
+              alt=""
+              width={15}
+              className="absolute -top-10 right-[9.5rem] md:hidden"
+            />
+          </div>
+          <p className="text-white my-7 md:hidden">
+            Email us below to any question related to our event
+          </p>
 
-					<form action="" method="post" className="flex flex-col gap-y-5">
-						<input
-							type="text"
-							placeholder="First Name"
-							className="w-full h-[47px] rounded-md pl-7 border border-[rgba(255, 255, 255, 1)] bg-transparent placeholder:text-white drop-shadow-[0px_4px_4px_0px rgba(0, 0, 0, 0.25)] text-white"
-						/>
-						<input
-							type="email"
-							placeholder="Email"
-							className="w-full h-[47px] rounded-md pl-7 border border-[rgba(255, 255, 255, 1)] bg-transparent placeholder:text-white drop-shadow-[0px_4px_4px_0px rgba(0, 0, 0, 0.25)] text-white"
-						/>
+          <form
+            action=""
+            method="post"
+            className="flex flex-col gap-y-5 md:mt-8 relative"
+          >
+            <input
+              type="text"
+              placeholder="First Name"
+              className="w-full h-[47px] rounded-md pl-7 border border-[rgba(255, 255, 255, 1)] bg-transparent placeholder:text-white drop-shadow-[0px_4px_4px_0px rgba(0, 0, 0, 0.25)] text-white"
+            />
+            <input
+              type="email"
+              placeholder="Email"
+              className="w-full h-[47px] rounded-md pl-7 border border-[rgba(255, 255, 255, 1)] bg-transparent placeholder:text-white drop-shadow-[0px_4px_4px_0px rgba(0, 0, 0, 0.25)] text-white"
+            />
 
-						<textarea
-							name="message"
-							id=""
-							cols="30"
-							rows="5"
-							placeholder="Message"
-							className="w-full rounded-md mb-5 pl-7 pt-7 border border-[rgba(255, 255, 255, 1)] bg-transparent placeholder:text-white text-white drop-shadow-[0px_4px_4px_0px rgba(0, 0, 0, 0.25)]"
-						/>
+            <textarea
+              name="message"
+              id=""
+              cols="30"
+              rows="5"
+              placeholder="Message"
+              className="w-full rounded-md pl-7 pt-7 border border-[rgba(255, 255, 255, 1)] bg-transparent placeholder:text-white text-white drop-shadow-[0px_4px_4px_0px rgba(0, 0, 0, 0.25)]"
+            />
 
-						<div className="flex justify-center">
-							<Button text={"Submit"} />
-						</div>
-					</form>
+            <div className="flex justify-center">
+              <button
+                type="submit"
+                className="text-white text-xl bg-gradient-to-r from-pink-500 via-purple-500 py-4 px-16 text-center rounded-md hover:bg-pink-500 transition-all duration-500 w-fit"
+              >
+                Submit
+              </button>
+            </div>
 
-					<div className="md:hidden">
-						<p className="text-color-2 text-center mt-10 mb-2">Share on</p>
-						<div className="mb-10">
-							<Socials />
-						</div>
-					</div>
-				</div>
-			</section>
+            <Image
+              src={sataGra}
+              alt=""
+              width={12}
+              className="absolute bottom-5 -left-3 md:-left-16 lg:-left-[3.2rem] md:w-[18px]"
+            />
 
-			{/* <Image
-        src={star}
-        alt=""
-        width={20}
-        className="absolute right-10 bottom-[19vh] md:right-14 md:bottom-[8vh] lg:bottom-[13vh]"
-      />
+            <Image
+              src={star}
+              alt=""
+              width={12}
+              className="absolute bottom-[5.5rem] -right-4 md:-bottom-10 md:-right-24 md:w-[15px]"
+            />
+          </form>
 
-      <Image
-        src={stargray}
-        alt=""
-        width={20}
-        className="absolute top-[23vh] right-12"
-      />
-      <Image
-        src={sataGra}
-        alt=""
-        width={15}
-        className="absolute top-[12vh] left-[35vh]"
-      />
-
-      <Image
-        src={sataGra}
-        alt=""
-        width={15}
-        className="absolute bottom-[6vh] left-[10vh] md:bottom-[30px] md:left-[64vh]"
-      /> */}
-		</main>
-	);
+          <div className="md:hidden">
+            <p className="text-color-2 text-center mt-10 mb-2">Share on</p>
+            <div className="mb-10">
+              <Socials />
+            </div>
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export default page;
--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,185 +1,186 @@
 import React from "react";
 
 import Image from "next/image";
-import graphicsDesginer from "../../../public/3d-graphic-designer.png";
+import graphicsDesginer from "../../../public/svg/3d-graphic-designer.svg";
 import movingLady from "../../../public/moving-lady.png";
 import movingMan from "../../../public/moving-man.png";
-import Button from "@/components/Button";
+import Modal from "@/components/Modal";
 
 const page = () => {
-	return (
-		<>
-			<main className="w-[70%] md:w-[95%] mx-auto">
-				<h2 className="md:hidden text-color-2 font-bold text-xl mt-5">
-					Register
-				</h2>
-				<section className="md:flex items-center ">
-					<div className="md:flex-0 lg:flex-1 mt-12 flex justify-center">
-						<div className="relative w-52 h-52 md:w-80 md:h-80 lg:w-96 lg:h-96 ">
-							<Image
-								src={graphicsDesginer}
-								alt="3D Graphics Designer Showing Thumbs Up"
-								fill={true}
-								className="object-cover"
-							/>
-						</div>
-					</div>
+  return (
+    <>
+      <main className="w-[70%] md:w-[95%] mx-auto">
+        <h2 className="md:hidden text-color-2 font-bold text-xl mt-5">
+          Register
+        </h2>
+        <section className="md:flex items-center ">
+          <div className="md:flex-0 lg:flex-1 mt-12 flex justify-center">
+            <div className="relative w-52 h-52 md:w-80 md:h-80 lg:w-96 lg:h-96 ">
+              <Image
+                src={graphicsDesginer}
+                alt="3D Graphics Designer Showing Thumbs Up"
+                fill={true}
+                className="object-cover"
+              />
+            </div>
+          </div>
 
-					<div className="md:flex-1 mt-8 text-white">
-						<div className="md:border md:p-14 lg:p-10 md:rounded-md md:border-transparent md:shadow-2xl md:shadow-[0px_4px_4px_0px rgba(0, 0, 0, 0.25)]">
-							<div className="flex items-center">
-								<p className="mb-2">Be part of this movement!</p>
+          <div className="md:flex-1 mt-8 text-white">
+            <div className="md:border md:p-14 lg:p-10 md:rounded-md md:border-transparent md:shadow-2xl md:shadow-[0px_4px_4px_0px rgba(0, 0, 0, 0.25)]">
+              <div className="flex items-center">
+                <p className="mb-2">Be part of this movement!</p>
 
-								<div className="flex border-b border-dashed border-purple-800 ">
-									<Image
-										src={movingLady}
-										alt="moving lady"
-										className="object-contain"
-									/>
-									<Image
-										src={movingMan}
-										alt="moving man"
-										className="object-contain"
-									/>
-								</div>
-							</div>
-							<h2 className="text-2xl mt-2 mb-7">CREATE YOUR ACCOUNT</h2>
+                <div className="flex border-b border-dashed border-purple-800 ">
+                  <Image
+                    src={movingLady}
+                    alt="moving lady"
+                    className="object-contain"
+                  />
+                  <Image
+                    src={movingMan}
+                    alt="moving man"
+                    className="object-contain"
+                  />
+                </div>
+              </div>
+              <h2 className="text-2xl mt-2 mb-7">CREATE YOUR ACCOUNT</h2>
 
-							<form action="" method="post">
-								<div className="flex flex-col gap-5 md:gap-3 md:grid grid-cols-2 grid-rows-2">
-									<div className="space-y-2 col-span-1">
-										<label htmlFor="teamName">{"Team's Name"}</label>
-										<input
-											type="text"
-											name="teamName"
-											placeholder="Enter the name of your group"
-											className="w-full h-[47px] rounded-md pl-7 md:pl-2 border-2 border-[rgba(255, 255, 255, 1)] bg-transparent drop-shadow-[0px_4px_4px_0px rgba(0, 0, 0, 0.25)] text-white md:placeholder:text-xs lg:placeholder:text-base"
-										/>
-									</div>
+              <form action="" method="post">
+                <div className="flex flex-col gap-5 md:gap-3 md:grid grid-cols-2 grid-rows-2">
+                  <div className="space-y-2 col-span-1">
+                    <label htmlFor="teamName">{"Team's Name"}</label>
+                    <input
+                      type="text"
+                      name="teamName"
+                      placeholder="Enter the name of your group"
+                      className="w-full h-[47px] rounded-md pl-7 md:pl-2 border-2 border-[rgba(255, 255, 255, 1)] bg-transparent drop-shadow-[0px_4px_4px_0px rgba(0, 0, 0, 0.25)] text-white md:placeholder:text-xs xl:placeholder:text-base"
+                    />
+                  </div>
 
-									<div className="space-y-2 col-span-1">
-										<label htmlFor="phone">Phone</label>
-										<input
-											type="phone"
-											name="phone"
-											placeholder="Enter your phone number "
-											className="w-full h-[47px] rounded-md pl-7 md:pl-2 border-2 border-[rgba(255, 255, 255, 1)] bg-transparent drop-shadow-[0px_4px_4px_0px rgba(0, 0, 0, 0.25)] text-white md:placeholder:text-xs lg:placeholder:text-base"
-										/>
-									</div>
+                  <div className="space-y-2 col-span-1">
+                    <label htmlFor="phone">Phone</label>
+                    <input
+                      type="phone"
+                      name="phone"
+                      placeholder="Enter your phone number "
+                      className="w-full h-[47px] rounded-md pl-7 md:pl-2 border-2 border-[rgba(255, 255, 255, 1)] bg-transparent drop-shadow-[0px_4px_4px_0px rgba(0, 0, 0, 0.25)] text-white md:placeholder:text-xs xl:placeholder:text-base"
+                    />
+                  </div>
 
-									<div className="space-y-2">
-										<label htmlFor="email">Email</label>
-										<input
-											type="email"
-											name="email"
-											placeholder="Enter your email address "
-											className="w-full h-[47px] rounded-md pl-7 md:pl-2 border-2 border-[rgba(255, 255, 255, 1)] bg-transparent drop-shadow-[0px_4px_4px_0px rgba(0, 0, 0, 0.25)] text-white md:placeholder:text-xs lg:placeholder:text-base"
-										/>
-									</div>
+                  <div className="space-y-2">
+                    <label htmlFor="email">Email</label>
+                    <input
+                      type="email"
+                      name="email"
+                      placeholder="Enter your email address "
+                      className="w-full h-[47px] rounded-md pl-7 md:pl-2 border-2 border-[rgba(255, 255, 255, 1)] bg-transparent drop-shadow-[0px_4px_4px_0px rgba(0, 0, 0, 0.25)] text-white md:placeholder:text-xs xl:placeholder:text-base"
+                    />
+                  </div>
 
-									<div className="space-y-2">
-										<label htmlFor="projectTopic">Project Topic</label>
-										<input
-											type="text"
-											name="projectTopic"
-											placeholder="What is your group project topic "
-											className="w-full h-[47px] rounded-md pl-7 md:pl-2 border-2 border-[rgba(255, 255, 255, 1)] bg-transparent drop-shadow-[0px_4px_4px_0px rgba(0, 0, 0, 0.25)] text-white md:placeholder:text-xs lg:placeholder:text-base"
-										/>
-									</div>
+                  <div className="space-y-2">
+                    <label htmlFor="projectTopic">Project Topic</label>
+                    <input
+                      type="text"
+                      name="projectTopic"
+                      placeholder="What is your group project topic "
+                      className="w-full h-[47px] rounded-md pl-7 md:pl-2 border-2 border-[rgba(255, 255, 255, 1)] bg-transparent drop-shadow-[0px_4px_4px_0px rgba(0, 0, 0, 0.25)] text-white md:placeholder:text-xs xl:placeholder:text-base"
+                    />
+                  </div>
 
-									<div className="flex gap-8 md:gap-2">
-										<div className="flex flex-col flex-1 md:flex-0 space-y-1">
-											<label htmlFor="category">Category</label>
-											<select
-												name="category"
-												id="category"
-												className="outline-white h-[2.7rem] rounded-md bg-transparent border-2 border-white text-white px-3 "
-											>
-												<option className="text-black md:text-base" value="">
-													Select your category
-												</option>
-												<option className="text-black" value="entertainment">
-													Business
-												</option>
-												<option className="text-black" value="technology">
-													Technology
-												</option>
-											</select>
-										</div>
+                  <div className="flex gap-8 md:gap-2">
+                    <div className="flex flex-col flex-1 md:flex-0 space-y-1">
+                      <label htmlFor="category">Category</label>
+                      <select
+                        name="category"
+                        id="category"
+                        className="outline-white h-[2.7rem] rounded-md bg-transparent border-2 border-white text-white px-3 "
+                      >
+                        <option className="text-black md:text-base" value="">
+                          Select
+                        </option>
+                        <option className="text-black" value="entertainment">
+                          Business
+                        </option>
+                        <option className="text-black" value="technology">
+                          Technology
+                        </option>
+                      </select>
+                    </div>
 
-										<div className="md:hidden flex flex-col flex-0 md:flex-1 space-y-1">
-											<label htmlFor="groupSize">Group Size</label>
-											<select
-												name="groupSize"
-												id="groupSize"
-												className="outline-white h-[2.7rem] rounded-md bg-transparent border-2 border-white text-white px-4"
-											>
-												<option
-													className="text-black text-xs md:text-base"
-													value=""
-												>
-													Select
-												</option>
-												<option className="text-black" value="1-5">
-													1-5
-												</option>
-												<option className="text-black" value="6-20">
-													6-20
-												</option>
-											</select>
-										</div>
-									</div>
+                    <div className="md:hidden flex flex-col flex-0 md:flex-1 space-y-1">
+                      <label htmlFor="groupSize">Group Size</label>
+                      <select
+                        name="groupSize"
+                        id="groupSize"
+                        className="outline-white h-[2.7rem] rounded-md bg-transparent border-2 border-white text-white px-4"
+                      >
+                        <option className="text-black" value="">
+                          Select
+                        </option>
+                        <option className="text-black" value="1-5">
+                          1-5
+                        </option>
+                        <option className="text-black" value="6-20">
+                          6-20
+                        </option>
+                      </select>
+                    </div>
+                  </div>
 
-									<div className="hidden md:flex flex-col space-y-1">
-										<label htmlFor="groupSize">Group Size</label>
-										<select
-											name="groupSize"
-											id="groupSize"
-											className="outline-white h-[2.7rem] rounded-md bg-transparent border-2 border-white text-white px-4"
-										>
-											<option
-												className="text-black text-xs md:text-base"
-												value=""
-											>
-												Select
-											</option>
-											<option className="text-black" value="1-5">
-												1-5
-											</option>
-											<option className="text-black" value="6-20">
-												6-20
-											</option>
-										</select>
-									</div>
-								</div>
+                  <div className="hidden md:flex flex-col space-y-1">
+                    <label htmlFor="groupSize">Group Size</label>
+                    <select
+                      name="groupSize"
+                      id="groupSize"
+                      className="outline-white h-[2.7rem] rounded-md bg-transparent border-2 border-white text-white px-4"
+                    >
+                      <option className="text-black" value="">
+                        Select
+                      </option>
+                      <option className="text-black" value="1-5">
+                        1-5
+                      </option>
+                      <option className="text-black" value="6-20">
+                        6-20
+                      </option>
+                    </select>
+                  </div>
+                </div>
 
-								<p className="text-xs italic text-[#FF26B9] my-5">
-									Please review your registration details before submitting
-								</p>
+                <p className="text-xs italic text-[#FF26B9] my-5">
+                  Please review your registration details before submitting
+                </p>
 
-								<div className="flex gap-4 pb-5">
-									<input
-										type="checkbox"
-										name="privacyPolicy"
-										id="privacyPolicy"
-										className="bg-transparent cursor-pointer"
-									/>
-									<p className="text-xs">
-										I agreed with the event terms and conditions and privacy
-										policy
-									</p>
-								</div>
+                <div className="flex gap-2">
+                  <input
+                    type="checkbox"
+                    name="privacyPolicy"
+                    id="privacyPolicy"
+                    className="bg-transparent cursor-pointer"
+                  />
+                  <p className="text-xs">
+                    I agreed with the event terms and conditions and privacy
+                    policy
+                  </p>
+                </div>
 
-								<div className="flex justify-center mt-5">
-									<Button text={"Submit"} />
-								</div>
-							</form>
-						</div>
-					</div>
-				</section>
-			</main>
-		</>
-	);
+                <div className="flex justify-center mt-5">
+                  <button
+                    type="submit"
+                    className="text-white text-xl bg-gradient-to-r from-pink-500 via-purple-500 py-4 px-16 text-center rounded-md hover:bg-pink-500 transition-all duration-500 w-fit"
+                  >
+                    Submit
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </section>
+      </main>
+
+      {/* <Modal /> */}
+    </>
+  );
 };
 
 export default page;
