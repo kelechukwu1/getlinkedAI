fd0dd16eca6f552a614fc5c7b622bfd5f8f439d0
thejuggernaut01 <adewaleolanrewaju2002@gmail.com>
2023-09-23 23:46:22 +0100

    wdd

--- src/store.js
+++ src/store.js
@@ -7,23 +7,24 @@ const isBrowser = typeof window !== "undefined";
 let parsedLocalStorageUser = [];
 
 if (isBrowser) {
-	const localStorageUser = localStorage.getItem("user");
-	parsedLocalStorageUser = localStorageUser ? JSON.parse(localStorageUser) : [];
+  const localStorageUser = localStorage.getItem("user");
+  parsedLocalStorageUser = localStorageUser ? JSON.parse(localStorageUser) : [];
 }
 const initialState = {
-	user: parsedLocalStorageUser, // Initial state for user
+  user: parsedLocalStorageUser, // Initial state for user
 };
 
 export const userSlice = createSlice({
-	name: "user",
-	initialState,
-	reducers: {
-		newUser: (state, action) => {
-			//set the state to action.payload
-			state.user = action.payload;
-			console.log(state.user);
-			localStorage.setItem("user", JSON.stringify(action.payload));
-		},
-	},
+  name: "user",
+  initialState,
+  reducers: {
+    newUser: (state, action) => {
+      //set the state to action.payload
+      state.user = action.payload;
+      console.log(state.user);
+      localStorage.setItem("user", JSON.stringify(action.payload));
+    },
+  },
 });
+
 export const { newUser } = userSlice.actions;
